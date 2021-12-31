891a645ea4486a510c6cf9ffc792fb2f5e028de4
Mahreen-Asama <79749919+Mahreen-Asama@users.noreply.github.com>
2021-12-31 12:25:06 +0500

    Add files via upload

---
+++ EmployeeMS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ClassLibraryInterface;
+
+namespace EMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EMS_Interface.startEMS();
+
+        }
+    }
+}
